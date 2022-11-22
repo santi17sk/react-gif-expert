be851d26834eb15f5563cb587e5b303c922e2fa3
Santi <santitito14@gmail.com>
2022-11-21 22:36:11 -0300

    Primer commit

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { GifExpertApp } from './GifExpertApp';
+// estilos globables
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <GifExpertApp />
+  </React.StrictMode>
+)
